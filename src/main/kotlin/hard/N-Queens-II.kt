249ce4a54a86e2ba896a495053896da66b356520
Vatsal <vatsalr23@gmail.com>
2021-08-07 00:50:29 -0700

    N-Queens-II

--- src/main/kotlin/hard/N-Queens-II.kt
+++ src/main/kotlin/hard/N-Queens-II.kt
@@ -1,34 +1,30 @@
 package hard
 
-class `N-Queens` {
-
-    fun solveNQueens(n: Int): List<List<String>> {
+class `N-Queens-II` {
+    fun totalNQueens(n: Int): Int {
         val queen = Queen(n)
-        val results = mutableListOf<List<String>>()
-        backtrack(queen, results, 0)
-        return results
+        return backtrack(queen, 0)
     }
 
-    private fun backtrack(queen: Queen, results: MutableList<List<String>>, row: Int) {
-        if (row > queen.boardSize) return
-        if (queen.queensPlaced == queen.boardSize) {
-            results.add(queen.board.map { String(it) })
-            return
-        }
+    private fun backtrack(queen: Queen, row: Int): Int {
+        if (row > queen.boardSize) return 0
+        if (queen.queensPlaced == queen.boardSize) return 1
 
+        var total = 0
         repeat(queen.boardSize) { col ->
             if (queen.canPlace(row, col)) {
                 queen.updatePosition(row, col, Queen.Move.PLACE)
-                backtrack(queen, results, row + 1)
+                total += backtrack(queen, row + 1)
                 queen.updatePosition(row, col, Queen.Move.REMOVE)
             }
         }
+
+        return total
     }
 
     class Queen(val boardSize: Int) {
 
         var queensPlaced = 0
-        var board = Array(boardSize) { CharArray(boardSize) { '.' } }
 
         enum class Move {
             PLACE,
@@ -39,16 +35,12 @@ class `N-Queens` {
         val antiDiagonals: BooleanArray = BooleanArray(2 * boardSize - 1) { false }
         val cols: BooleanArray = BooleanArray(boardSize) { false }
 
-        private fun updateBoard(row: Int, col: Int, move: Move) {
-            board[row][col] = when (move) {
-                Move.PLACE -> 'Q'
-                Move.REMOVE -> '.'
-            }
-        }
-
-        fun updatePosition(row: Int, col: Int, move: Move): Boolean {
+        fun updatePosition(row: Int, col: Int, move: Move) {
             val positionBoolean = when(move) {
                 Move.PLACE -> {
+                    if (!canPlace(row, col)) {
+                        return
+                    }
                     queensPlaced += 1
                     true
                 }
@@ -57,19 +49,16 @@ class `N-Queens` {
                     false
                 }
             }
-            updateBoard(row, col, move)
 
             antiDiagonals[row + col] = positionBoolean
             cols[col] = positionBoolean
             mainDiagonals[col - row + (boardSize - 1)] = positionBoolean
-
-            return true
         }
 
         fun canPlace(row: Int, col: Int): Boolean {
             return !antiDiagonals[row + col] &&
-                !cols[col] &&
-                !mainDiagonals[col - row + (boardSize - 1)]
+                    !cols[col] &&
+                    !mainDiagonals[col - row + (boardSize - 1)]
         }
     }
 }
